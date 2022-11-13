b68c1f94f3e2e1d8a5ac722ce56e08e31447ff13
Dany Cervantes <danycervantesq@gmail.com>
2022-11-13 00:43:10 -0500

    Update starship.js
    Random background image selection onload

--- starship.js
+++ starship.js
@@ -1,8 +1,38 @@
-let body = document.querySelector(".body");
-
 let images = [
-    { milkyway: "url(assets/milkyway.jpeg)",
-    
-}
+    { 
+        name: "Milky Way Galaxy",
+        img: "url(assets/milky-way.jpeg)",
+    },
+    {
+        name: "Distant Sun",
+        img: "url(assets/distant-sun.jpeg"
+    }
 ];
 
+
+//Returns a random number within a chosen range
+function randomRange(min,max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+//Math.floor() rounds down to the nearest whole number  e.i. 10 = 0 - 9  
+//Math.random() returns a random decimal between 0 - 0.99
+}
+
+
+function outerSpace() {
+    
+    let body = document.querySelector(".body");
+    let num = randomRange(0, images.length -1);
+
+    body.style.background = images[num].img;
+
+    body.style.backgroundPosition = "center";
+    body.style.backgroundRepeat = "no-repeat";
+    body.style.backgroundSize = "cover";
+}
+
+
+window.onload = function() {
+
+    outerSpace();
+
+}
